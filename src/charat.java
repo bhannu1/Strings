e6a857f5fc10ed7c9835c12caf4b3d41e491aa8d
bhannu1 <210303126160@paruluniversity.ac.in>
2023-02-07 14:23:37 +0530

    Create charat.java

---
+++ src/charat.java
@@ -0,0 +1,7 @@
+public class charat {
+    public static void main(String[] args) {
+                String name="Bhavya";
+                char ch=name.charAt(4);
+                System.out.println(ch);
+    }
+}
